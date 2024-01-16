ec593ccda2094a9a8dff0598f4916cc484021f3e
Yesil <yesil@gmail.com>
2024-01-16 17:50:37 +0300

    feat: blog show html

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from blogs.models import  Category,Blogs
 def home(request):
-    return  render(request,'home.html')
+    category=Category.objects.all()
+    featured_posts=Blogs.objects.filter(is_featured=True,status='Published').order_by('-update_at')
+    posts=Blogs.objects.filter(is_featured=False,status='Published')
+    context={
+        'category':category,
+        'featured_posts':featured_posts,
+        'posts':posts
+    }
+    return  render(request,'home.html',context)
