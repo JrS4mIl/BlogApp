88a9b64aea83aedbbb864e367b833769c73fc687
Yesil <yesil@gmail.com>
2024-01-19 15:33:09 +0300

    feat: dashboard category finish

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from blogs.models import Category,Blogs
 from django.contrib.auth.decorators import login_required
+from .form import CategoryForm
 # Create your views her
 @login_required(login_url='login')
 def dashboard(request):
@@ -16,3 +17,35 @@ def dashboard(request):
 @login_required(login_url='login')
 def categories(request):
     return render(request,'dashboard/categories.html')
+
+def add_category(request):
+    if request.method=='POST':
+        form=CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form=CategoryForm()
+    context={
+        'form':form
+    }
+    return render(request,'dashboard/add_category.html',context)
+
+def edit_category(request,pk):
+    category=get_object_or_404(Category,pk=pk)
+    if request.method=='POST':
+        form=CategoryForm(request.POST,instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form=CategoryForm(instance=category)
+
+    context={
+        'form':form,
+        'category':category
+    }
+    return render(request,'dashboard/edit_category.html',context)
+
+def delete_category(request,pk):
+    category=get_object_or_404(Category,pk=pk)
+    category.delete()
+    return redirect('categories')
